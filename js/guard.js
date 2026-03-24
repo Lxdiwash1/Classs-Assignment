fc7796510bcad1d825d754219c6cc85782d93f00
anilsinghmauni <anilsinghmauni@gmail.com>
2026-03-24 22:04:57 +0545

    fix routing issues

--- js/guard.js
+++ js/guard.js
@@ -1,8 +1,10 @@
 // Redirect if not logged in
 (function () {
+  console.log("guard file")
   const isAuth = localStorage.getItem("auth");
+  console.log("isAuth",isAuth)
 
-  if (!isAuth) {
+  if (isAuth == null || false) {
     window.location.href = "login.html";
   }
 })();
